7787d0fb106fa3d6e1830aea52ac3f8326cda9ee
Renan A. (iamrenan) <renan.aguiar17+mouts@gmail.com>
2026-08-24 01:35:34 -0300

    feat(Migrations): Add Sales Table Migrations

---
+++ template/backend/src/Ambev.DeveloperEvaluation.ORM/Migrations/20260824043048_AddSalesTable.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ambev.DeveloperEvaluation.ORM.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSalesTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "Total",
+                table: "SaleItems",
+                type: "numeric",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Total",
+                table: "SaleItems");
+        }
+    }
+}
