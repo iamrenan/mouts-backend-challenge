91f36d8137eaefa8554cbf09bd397fd8f01710d2
Renan A. (iamrenan) <renan.aguiar17+mouts@gmail.com>
2026-08-24 01:34:58 -0300

    feat(ORM): Configure Sale Entity mappings and repository
    - Fixes previously uncommited bug that set MigrationsAssembly to WebApi instead of ORM
    - Initial implementation of the ISaleRepository and new Sales and SaleItems entities configuration

--- template/backend/src/Ambev.DeveloperEvaluation.Domain/Repositories/ISaleRepository.cs
+++ template/backend/src/Ambev.DeveloperEvaluation.Domain/Repositories/ISaleRepository.cs
@@ -7,7 +7,7 @@ namespace Ambev.DeveloperEvaluation.Domain.Repositories
         Task<Sale> CreateAsync(Sale sale, CancellationToken cancellationToken = default);
         Task<Sale?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
         Task<List<Sale>> GetAllAsync(int pageNumber, int pageSize, CancellationToken cancellationToken = default);
-        Task<bool> UpdateAsync(Sale sale, CancellationToken cancellationToken = default);
+        Task<int> UpdateAsync(Sale sale, CancellationToken cancellationToken = default);
         Task<bool> DeleteAsync(Guid id, CancellationToken cancellationToken = default);
     }
 }
